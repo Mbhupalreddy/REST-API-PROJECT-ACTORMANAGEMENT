7ec5298c8b7a6c0b6c6f6fa9ea99ae13467900a5
BHUPAL REDDY <BHUPAL REDDY@LAPTOP-6APJ1DHF>
2026-05-21 18:27:14 +0530

    created entity class

---
+++ src/main/java/com/nt/SpringRestProj10MiniProject02CurdOperationApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestProj10MiniProject02CurdOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestProj10MiniProject02CurdOperationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nt/entity/ActorInfoEntity.java
@@ -0,0 +1,50 @@
+package com.nt.entity;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.Data;
+@Entity
+@Table(name="REST_ACTORINFO")
+@Data
+public class ActorInfoEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer aid;
+	
+	@Column(length=30)
+	private String aname;
+	@Column(length=30)
+	private String addrs;
+	
+	@Column
+	private Double remuneration;
+	@Column(length=10)
+	private String active_SW;
+	
+	//meta data
+	@Column(length=30)
+	private String createdBy;
+	@Column(length=30)
+	private String updatedBy;
+	
+	@CreationTimestamp
+	@Column(updatable = false,insertable = true)
+	private LocalDate creationDate;
+	
+	@UpdateTimestamp
+	@Column(insertable = false,updatable = true)
+	private LocalDate updationDate;
+	
+	@Version
+	private Integer updationCount;
+}
